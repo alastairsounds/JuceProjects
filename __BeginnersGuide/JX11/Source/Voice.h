f890cefa46fec37513adf5808661998910c6b762
Alastair Scheuermann <66482739+alastairsounds@users.noreply.github.com>
2025-10-08 18:21:10 -0600

    BG JX11: BLIT to sawtooth

--- __BeginnersGuide/JX11/Source/Voice.h
+++ __BeginnersGuide/JX11/Source/Voice.h
@@ -6,15 +6,19 @@ struct Voice
 {
     int note;
     Oscillator osc;
+    float saw;
 
     void reset()
     {
         note = 0;
+        saw = 0.0f;
         osc.reset();
     }
 
     float render()
     {
-        return osc.nextSample();
+        float sample = osc.nextSample();
+        saw = saw * 0.997f + sample;
+        return saw;
     }
 };
