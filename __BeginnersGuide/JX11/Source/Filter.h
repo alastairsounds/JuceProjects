989ef79b3d9f6a2ddeb8730be330c939fca16d53
Alastair Scheuermann <66482739+alastairsounds@users.noreply.github.com>
2025-11-13 11:21:20 +0000

    BG JX11: Moog ladder filter

--- __BeginnersGuide/JX11/Source/Filter.h
+++ __BeginnersGuide/JX11/Source/Filter.h
@@ -1,45 +1,18 @@
 #pragma once
-#include <cmath>
+#include <JuceHeader.h>
 
-class Filter
+class Filter : public juce::dsp::LadderFilter<float>
 {
 public:
-    float sampleRate;
-
     void updateCoefficients(float cutoff, float Q)
     {
-        g = std::tan(PI * cutoff / sampleRate);
-        k = 1.0f / Q;
-        a1 = 1.0f / (1.0f + g * (g + k));
-        a2 = g * a1;
-        a3 = g * a2;
-    }
-
-    void reset()
-    {
-        g = 0.0f;
-        k = 0.0f;
-        a1 = 0.0f;
-        a2 = 0.0f;
-        a3 = 0.0f;
-
-        ic1eq = 0.0f;
-        ic2eq = 0.0f;
+        setCutoffFrequencyHz(cutoff);
+        setResonance(std::clamp(Q / 30.0f, 0.0f, 1.0f));
     }
 
     float render(float x)
     {
-        float v3 = x - ic2eq;
-        float v1 = a1 * ic1eq + a2 * v3;
-        float v2 = ic2eq + a2 * ic1eq + a3 * v3;
-        ic1eq = 2.0f * v1 - ic1eq;
-        ic2eq = 2.0f * v2 - ic2eq;
-        return v2;
+        updateSmoothers();
+        return processSample(x, 0);
     }
-
-private:
-    const float PI = 3.1415926535897932f;
-
-    float g, k, a1, a2, a3;  // filter coefficients
-    float ic1eq, ic2eq;      // internal state
 };
