d53654d8724d7a826c1f0eff60b3ee2e4b99ad4a
Alastair Scheuermann <66482739+alastairsounds@users.noreply.github.com>
2025-11-20 17:42:04 -0700

    TAP Sampler: init commit

---
+++ __TAP/helloSampler/Source/PluginEditor.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "PluginProcessor.h"
+#include <JuceHeader.h>
+
+//==============================================================================
+/**
+*/
+class HelloSamplerAudioProcessorEditor : public juce::AudioProcessorEditor
+{
+public:
+    HelloSamplerAudioProcessorEditor (HelloSamplerAudioProcessor&);
+    ~HelloSamplerAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    HelloSamplerAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HelloSamplerAudioProcessorEditor)
+};
